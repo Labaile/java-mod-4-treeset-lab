09414e99cdebaed2734f3dd632d8f401d2a24f99
Labaile <Labaile1@gmail.com>
2022-07-19 04:21:45 -0400

    added treeset lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,20 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 1, 4, 5, 2, 34, 36, 5, 4, 23, 45, 6, 8, 56, 34, 37 };
+        int[] numbers = {1, 4, 5, 2, 34, 36, 5, 4, 23, 45, 6, 8, 56, 34, 37};
         ArrayList<Integer> res = getNums(numbers);
         System.out.println(res); // [34, 36, 37, 45, 56]
     }
 
-    public static ArrayList<Integer> getNums(int[] nums) {
+    public static TreeSet<Integer> getNums(int[] nums) {
         // your code here
-        return null;
+        TreeSet<Integer> numTreeSet = new TreeSet<>();
+        ArrayList<Integer> numList = new ArrayList<>();
+        for (Integer num : nums) {
+           numTreeSet.tailSet(25);
+        }
+        return numList;
+        //return null
+
     }
 }
